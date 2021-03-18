03a80cff8825bd29ec24d851bd215d5740ccefb3
AVR <ajvladimir@gmail.com>
2021-03-17 18:01:31 -0700

    removed some stuff

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,15 @@
-import Spinner from "react-bootstrap/Spinner";
 import Head from "next/head";
-import React from "react";
 
 export default function Home() {
-  const [load, setLoad] = React.useState(false);
-
-  React.useEffect(() => {
-    setLoad(true);
-  }, []);
   return (
     <>
       <Head>
         <title>Musaeum Analytics</title>
       </Head>
-      {load ? (
-        <iframe
-          src="https://drive.google.com/file/d/1IPiQGgF7qMJCHdTtEnjB-s6TbvIOZMCN/preview"
-          title="Musaeum Analytics"
-        />
-      ) : (
-        <div className="centerItem">
-          <Spinner animation="grow" variant="dark" />
-          <Spinner animation="grow" variant="dark" />
-          <Spinner animation="grow" variant="dark" />
-        </div>
-      )}
+      <iframe
+        src="https://drive.google.com/file/d/1IPiQGgF7qMJCHdTtEnjB-s6TbvIOZMCN/preview"
+        title="Musaeum Analytics"
+      />
     </>
   );
 }
